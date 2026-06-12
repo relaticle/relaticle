53cc2e0d9777d8fdf4c77c032f8b58e323aadc28
Manuk <manuk.minasyan1@gmail.com>
2026-06-12 20:57:37 +0400

    feat(auth): lead the login passwordless cluster with the passkey button

---
+++ resources/views/components/auth/or-divider.blade.php
@@ -0,0 +1,5 @@
+<div class="flex items-center justify-center my-4">
+    <div class="w-full border-t border-gray-300 dark:border-gray-700"></div>
+    <span class="mx-2 text-gray-500 dark:text-gray-400">{{ __('or') }}</span>
+    <div class="w-full border-t border-gray-300 dark:border-gray-700"></div>
+</div>
