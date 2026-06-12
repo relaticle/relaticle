180b4f8bd51b84a3d7577a8811c964330b9ec99e
AsmitNepali <mail2asmitnepali@gmail.com>
2026-06-12 23:16:52 +0545

    fix: specify generic types on ActivityLog TeamScope (phpstan)

--- app/Models/ActivityLog/Scopes/TeamScope.php
+++ app/Models/ActivityLog/Scopes/TeamScope.php
@@ -9,8 +9,18 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 
+/**
+ * @template TModel of Model
+ *
+ * @implements Scope<TModel>
+ */
 final class TeamScope implements Scope
 {
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  Builder<covariant TModel>  $builder
+     */
     public function apply(Builder $builder, Model $model): void
     {
         $tenantId = Filament::getTenant()?->getKey();
