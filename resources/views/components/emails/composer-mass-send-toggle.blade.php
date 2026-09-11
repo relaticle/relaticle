3059a33d1c16812ec8691d84531123769d59e8cd
Asmit Nepali <mail2asmitnepali@gmail.com>
2026-09-12 00:34:11 +0545

    Link the mass send toggle label so clicking it toggles the switch.

--- resources/views/components/emails/composer-mass-send-toggle.blade.php
+++ resources/views/components/emails/composer-mass-send-toggle.blade.php
@@ -3,8 +3,8 @@
 ])
 
 <div {{ $attributes->class(['flex items-center gap-1.5']) }}>
-    <x-filament::toggle :state="$state" class="shrink-0 scale-75" />
-    <span class="text-xs text-gray-700 dark:text-gray-200">
+    <x-filament::toggle :state="$state" id="composer-mass-send-toggle" class="shrink-0 scale-75" />
+    <label for="composer-mass-send-toggle" class="cursor-pointer text-xs text-gray-700 dark:text-gray-200">
         {{ __('filament/emails/composer.mass_send.toggle') }}
-    </span>
+    </label>
 </div>
